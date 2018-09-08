89bdc7ef76260a88444044cf1cc6303800adc06b
Adam Carroll <oceloteater@users.noreply.github.com>
2018-09-09 00:54:43 +0200

    Ripping out boilerplate and getting token from local storage

---
+++ client/app/utils/storage.js
@@ -0,0 +1,28 @@
+export function getFromStorage(key) {
+  if (!key) {
+    console.log("Error: Key is missing");
+    return null;
+  } else {
+    try {
+    const valueStr = localStorage.getItem(key);
+    if (valueStr) {
+      return JSON.parse(valueStr);
+    }
+    } catch (error) {
+      console.log("Error: " + error);
+      return null;
+    }
+  }
+}
+
+export function setFromStorage(key, object) {
+  if (!key) {
+    console.log("Error: Key is missing");
+  } else {
+    try {
+      localStorage.setItem(key, JSON.stringify(object));
+    } catch (error) {
+      console.log("Could not stringify object: " + error);
+    }
+  }
+}
